cdbf0eb25af1b3e424eb85cfae03e150d1244111
Aldo Raine <105580292+Lt-AldoRaine@users.noreply.github.com>
2023-11-25 03:30:21 -0600

    fix test files

--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -1,105 +1,107 @@
 import Gameboard from "../modules/Gameboard";
 import Ship from "../modules/Ship";
-import { expect, test } from "vitest";
+import { describe, expect, it } from "vitest";
 
-test("Ship placed on board", () => {
-  const gameboard = new Gameboard();
-  const carrier = new Ship("carrier");
+describe("Gameboard", () => {
+  it("Ship placed on board", () => {
+    const gameboard = new Gameboard();
+    const carrier = new Ship("carrier");
 
-  expect(gameboard.placeShip(carrier, { x: 2, y: 2 }, false)).toBe(true);
-  expect(gameboard.shipCount).toBe(1);
-});
+    expect(gameboard.placeShip(carrier, { x: 2, y: 2 }, false)).toBe(true);
+    expect(gameboard.shipCount).toBe(1);
+  });
 
-test("Ship placed vertically", () => {
-  const gameboard = new Gameboard();
-  const carrier = new Ship("carrier");
+  it("Ship placed vertically", () => {
+    const gameboard = new Gameboard();
+    const carrier = new Ship("carrier");
 
-  expect(gameboard.placeShip(carrier, { x: 2, y: 1 }, true)).toBe(true);
-  expect(gameboard.shipCount).toBe(1);
-});
+    expect(gameboard.placeShip(carrier, { x: 2, y: 1 }, true)).toBe(true);
+    expect(gameboard.shipCount).toBe(1);
+  });
 
-test("Ship does not fit horizontally", () => {
-  const gameboard = new Gameboard();
-  const carrier = new Ship("carrier");
+  it("Ship does not fit horizontally", () => {
+    const gameboard = new Gameboard();
+    const carrier = new Ship("carrier");
 
-  expect(gameboard.placeShip(carrier, { x: 2, y: 8 }, false)).toBe(false);
-  expect(gameboard.shipCount).toBe(0);
-});
+    expect(gameboard.placeShip(carrier, { x: 2, y: 8 }, false)).toBe(false);
+    expect(gameboard.shipCount).toBe(0);
+  });
 
-test("Ship does not fit vertically", () => {
-  const gameboard = new Gameboard();
-  const carrier = new Ship("carrier");
+  it("Ship does not fit vertically", () => {
+    const gameboard = new Gameboard();
+    const carrier = new Ship("carrier");
 
-  expect(gameboard.placeShip(carrier, { x: 7, y: 2 }, true)).toBe(false);
-  expect(gameboard.shipCount).toBe(0);
-});
+    expect(gameboard.placeShip(carrier, { x: 7, y: 2 }, true)).toBe(false);
+    expect(gameboard.shipCount).toBe(0);
+  });
 
-test("Ships cannot overlap", () => {
-  const gameboard = new Gameboard();
-  const carrier = new Ship("carrier");
-  const submarine = new Ship("submarine");
+  it("Ships cannot overlap", () => {
+    const gameboard = new Gameboard();
+    const carrier = new Ship("carrier");
+    const submarine = new Ship("submarine");
 
-  gameboard.placeShip(carrier, { x: 0, y: 1 });
+    gameboard.placeShip(carrier, { x: 0, y: 1 });
 
-  expect(gameboard.placeShip(submarine, { x: 0, y: 2 })).toBe(false);
-  expect(gameboard.shipCount).toBe(1);
-});
+    expect(gameboard.placeShip(submarine, { x: 0, y: 2 })).toBe(false);
+    expect(gameboard.shipCount).toBe(1);
+  });
 
-test("Cell contains ship", () => {
-  const gameboard = new Gameboard();
-  const carrier = new Ship("carrier");
+  it("Cell contains ship", () => {
+    const gameboard = new Gameboard();
+    const carrier = new Ship("carrier");
 
-  gameboard.placeShip(carrier, { x: 0, y: 1 });
+    gameboard.placeShip(carrier, { x: 0, y: 1 });
 
-  expect(gameboard.cellContainsShip({ x: 0, y: 1 })).toBe(true);
-});
+    expect(gameboard.cellContainsShip({ x: 0, y: 1 })).toBe(true);
+  });
 
-test("Board recieves attack", () => {
-  const gameboard = new Gameboard();
+  it("Board recieves attack", () => {
+    const gameboard = new Gameboard();
 
-  expect(gameboard.recieveAttack({ x: 0, y: 2 })).toBe(true);
-});
+    expect(gameboard.recieveAttack({ x: 0, y: 2 })).toBe(true);
+  });
 
-test("Attack is out of bounds", () => {
-  const gameboard = new Gameboard();
+  it("Attack is out of bounds", () => {
+    const gameboard = new Gameboard();
 
-  expect(gameboard.recieveAttack({ x: 10, y: 10 })).toBe(false);
-});
+    expect(gameboard.recieveAttack({ x: 10, y: 10 })).toBe(false);
+  });
 
-test("Attack hits ship", () => {
-  const gameboard = new Gameboard();
-  const submarine = new Ship("submarine");
+  it("Attack hits ship", () => {
+    const gameboard = new Gameboard();
+    const submarine = new Ship("submarine");
 
-  console.log(submarine.hits);
+    console.log(submarine.hits);
 
-  gameboard.placeShip(submarine, { x: 1, y: 3 });
+    gameboard.placeShip(submarine, { x: 1, y: 3 });
 
-  expect(gameboard.recieveAttack({ x: 1, y: 3 })).toBe(true);
-  expect(submarine.hits).toBe(1);
-});
+    expect(gameboard.recieveAttack({ x: 1, y: 3 })).toBe(true);
+    expect(submarine.hits).toBe(1);
+  });
 
-test("Attack sinks ship", () => {
-  const gameboard = new Gameboard();
-  const patrolBoat = new Ship("patrol boat");
+  it("Attack sinks ship", () => {
+    const gameboard = new Gameboard();
+    const patrolBoat = new Ship("patrol boat");
 
-  gameboard.placeShip(patrolBoat, { x: 0, y: 1 });
-  gameboard.recieveAttack({ x: 0, y: 1 });
-  gameboard.recieveAttack({ x: 0, y: 2 });
+    gameboard.placeShip(patrolBoat, { x: 0, y: 1 });
+    gameboard.recieveAttack({ x: 0, y: 1 });
+    gameboard.recieveAttack({ x: 0, y: 2 });
 
-  patrolBoat.isSunk();
+    patrolBoat.isSunk();
 
-  expect(patrolBoat.sunk).toBe(true);
-});
+    expect(patrolBoat.sunk).toBe(true);
+  });
 
-test("Detects all ships sunk", () => {
-  const gameboard = new Gameboard();
-  const patrolBoat = new Ship("patrol boat");
+  it("Detects all ships sunk", () => {
+    const gameboard = new Gameboard();
+    const patrolBoat = new Ship("patrol boat");
 
-  gameboard.placeShip(patrolBoat, { x: 0, y: 1 });
-  gameboard.recieveAttack({ x: 0, y: 1 });
-  gameboard.recieveAttack({ x: 0, y: 2 });
+    gameboard.placeShip(patrolBoat, { x: 0, y: 1 });
+    gameboard.recieveAttack({ x: 0, y: 1 });
+    gameboard.recieveAttack({ x: 0, y: 2 });
 
-  gameboard.checkShipsSunk();
+    gameboard.checkShipsSunk();
 
-  expect(gameboard.allShipsSunk).toBe(true);
+    expect(gameboard.allShipsSunk).toBe(true);
+  });
 });
--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -1,33 +1,35 @@
-import { expect, test } from "vitest";
+import { expect, describe, it } from "vitest";
 import Ship from "../modules/Ship";
 
-test("Ship gets hit", () => {
-  const ship = new Ship("carrier");
-  ship.hit();
+describe("Ship", () => {
+  it("Ship gets hit", () => {
+    const ship = new Ship("carrier");
+    ship.hit();
 
-  expect(ship.hits).toEqual(1);
-});
+    expect(ship.hits).toEqual(1);
+  });
 
-test("Ship takes multiple hits", () => {
-  const ship = new Ship("carrier");
-  ship.hit();
-  ship.hit();
+  it("Ship takes multiple hits", () => {
+    const ship = new Ship("carrier");
+    ship.hit();
+    ship.hit();
 
-  expect(ship.hits).toEqual(2);
-});
+    expect(ship.hits).toEqual(2);
+  });
 
-test("Ship gets sunk", () => {
-  const ship = new Ship("patrol boat");
-  ship.hit();
-  ship.hit();
+  it("Ship gets sunk", () => {
+    const ship = new Ship("patrol boat");
+    ship.hit();
+    ship.hit();
 
-  ship.isSunk();
+    ship.isSunk();
 
-  expect(ship.sunk).toBe(true);
-});
+    expect(ship.sunk).toBe(true);
+  });
 
-test("Ship has correct length", () => {
-  const carrier = new Ship("carrier");
+  it("Ship has correct length", () => {
+    const carrier = new Ship("carrier");
 
-  expect(carrier.length).toBe(5);
+    expect(carrier.length).toBe(5);
+  });
 });
