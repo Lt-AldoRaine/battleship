77906fc4df1a33b0ba7a121b068d203dcd858265
Aldo Raine <105580292+Lt-AldoRaine@users.noreply.github.com>
2023-11-25 02:44:21 -0600

    feat: create functions to recieve attacks on board

--- modules/Gameboard.js
+++ modules/Gameboard.js
@@ -3,39 +3,86 @@ import Ship from "./Ship";
 export default class Gameboard {
   constructor() {
     this.board = new Array(10).fill().map(() => new Array(10).fill(" "));
+    this.ships = [];
     this.shipCount = 0;
     this.allShipsSunk = false;
+    this.hitAttacks = 0;
   }
 
   placeShip(ship, coords, isVertical = false) {
     let rowStart = coords.x;
     let colStart = coords.y;
+    let shipCoords = [];
     const shipLength = ship.length;
-    const shipLetter = ship.name.charAt(0).toUpperCase();
 
     if (!isVertical) {
       if (10 - colStart >= shipLength) {
         for (let i = 0; i < shipLength; i++) {
-          this.board[rowStart].splice(colStart++, 1, shipLetter);
+          // prettier-ignore
+          if (this.cellContainsShip({ x: rowStart, y: colStart })) return false;
+          shipCoords.push(colStart);
+          this.board[rowStart].splice(colStart++, 1, ship);
         }
+        this.ships.push(ship);
         this.shipCount += 1;
         return true;
       } else return false;
     } else {
       if (10 - rowStart >= shipLength) {
         for (let i = 0; i < shipLength; i++) {
-          this.board[rowStart++].splice(colStart, 1, shipLetter);
+          // prettier-ignore
+          if (this.cellContainsShip({ x: rowStart, y: colStart })) return false;
+          this.board[rowStart++].splice(colStart, 1, ship);
         }
+        this.ships.push(ship);
         this.shipCount += 1;
         return true;
       } else return false;
     }
   }
+
+  cellContainsShip(coords) {
+    if (this.board[coords.x][coords.y] !== " ") {
+      return true;
+    } else return false;
+  }
+
+  recieveAttack(coords) {
+    const validX = coords.x < 10 && coords.x >= 0;
+    const validY = coords.y < 10 && coords.y >= 0;
+
+    if (validX && validY) {
+      if (this.cellContainsShip(coords)) {
+        const hitShip = this.board[coords.x][coords.y];
+        hitShip.hit();
+        return true;
+      } else if (!this.cellContainsShip(coords)) {
+        this.missedAttacks += 1;
+        return true;
+      }
+    } else return false;
+  }
+
+  checkShipsSunk() {
+    this.ships.forEach((ship) => {
+      if (ship.sunk) {
+        this.shipCount -= 1;
+      }
+    });
+
+    if (this.shipCount === 0) {
+      this.allShipsSunk = true;
+    }
+  }
 }
 
-let b = new Gameboard();
-let carrier = new Ship("carrier");
+let bodr = new Gameboard();
+const submarine = new Ship("submarine");
 
-b.placeShip(carrier, { x: 0, y: 0 }, true);
+bodr.placeShip(submarine, { x: 0, y: 2 });
 
-console.log(b.board);
+bodr.recieveAttack({ x: 0, y: 2 });
+bodr.recieveAttack({ x: 0, y: 3 });
+bodr.recieveAttack({ x: 0, y: 4 });
+
+console.log(bodr.board);
--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -30,14 +30,34 @@ test("Ship does not fit vertically", () => {
   const gameboard = new Gameboard();
   const carrier = new Ship("carrier");
 
-  expect(gameboard.placeShip(carrier, { x: 8, y: 2 }, true)).toBe(false);
+  expect(gameboard.placeShip(carrier, { x: 7, y: 2 }, true)).toBe(false);
   expect(gameboard.shipCount).toBe(0);
+});
+
+test("Ships cannot overlap", () => {
+  const gameboard = new Gameboard();
+  const carrier = new Ship("carrier");
+  const submarine = new Ship("submarine");
+
+  gameboard.placeShip(carrier, { x: 0, y: 1 });
+
+  expect(gameboard.placeShip(submarine, { x: 0, y: 2 })).toBe(false);
+  expect(gameboard.shipCount).toBe(1);
+});
+
+test("Cell contains ship", () => {
+  const gameboard = new Gameboard();
+  const carrier = new Ship("carrier");
+
+  gameboard.placeShip(carrier, { x: 0, y: 1 });
+
+  expect(gameboard.cellContainsShip({ x: 0, y: 1 })).toBe(true);
 });
 
 test("Board recieves attack", () => {
   const gameboard = new Gameboard();
 
-  expect(gameboard.recieveAttack({ x: 3, y: 2 })).toBe(true);
+  expect(gameboard.recieveAttack({ x: 0, y: 2 })).toBe(true);
 });
 
 test("Attack is out of bounds", () => {
@@ -50,7 +70,36 @@ test("Attack hits ship", () => {
   const gameboard = new Gameboard();
   const submarine = new Ship("submarine");
 
+  console.log(submarine.hits);
+
   gameboard.placeShip(submarine, { x: 1, y: 3 });
 
   expect(gameboard.recieveAttack({ x: 1, y: 3 })).toBe(true);
+  expect(submarine.hits).toBe(1);
+});
+
+test("Attack sinks ship", () => {
+  const gameboard = new Gameboard();
+  const patrolBoat = new Ship("patrol boat");
+
+  gameboard.placeShip(patrolBoat, { x: 0, y: 1 });
+  gameboard.recieveAttack({ x: 0, y: 1 });
+  gameboard.recieveAttack({ x: 0, y: 2 });
+
+  patrolBoat.isSunk();
+
+  expect(patrolBoat.sunk).toBe(true);
+});
+
+test("Detects all ships sunk", () => {
+  const gameboard = new Gameboard();
+  const patrolBoat = new Ship("patrol boat");
+
+  gameboard.placeShip(patrolBoat, { x: 0, y: 1 });
+  gameboard.recieveAttack({ x: 0, y: 1 });
+  gameboard.recieveAttack({ x: 0, y: 2 });
+
+  gameboard.checkShipsSunk();
+
+  expect(gameboard.allShipsSunk).toBe(true);
 });
